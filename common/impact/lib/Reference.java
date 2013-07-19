1742c21abc289d0c9a42ac8ff8055ce56c3b9dc0
jakejones48 <jakejones48@hotmail.com>
2013-07-19 18:11:16 +1000

    Initial files to start the mod

---
+++ common/impact/lib/Reference.java
@@ -0,0 +1,8 @@
+package impact.lib;
+
+public class Reference {
+	public static final String MOD_ID = "Impact";
+	public static final String MOD_NAME = "Impact";
+	public static final String VERSION = "0.0.1";
+
+}
