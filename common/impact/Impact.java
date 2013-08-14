e8970b564bad86619e79416b73d5b66539e74179
jakejones48 <jakejones48@hotmail.com>
2013-08-14 14:59:23 +1000

    Will probably get reversed in the future hopefully

--- common/impact/Impact.java
+++ common/impact/Impact.java
@@ -4,11 +4,15 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.StepSound;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.src.ModLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ChunkEvent;
+import impact.block.ImpactBlockFurnace;
 import impact.lib.Reference;
-
+import impact.world.biome.TimeChecker;
 import impact.world.storage.DataInitHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
@@ -19,6 +23,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class Impact {
@@ -33,8 +38,14 @@ public class Impact {
 	@Init
 	public void init(FMLInitializationEvent event){
 		MinecraftForge.EVENT_BUS.register(new DataInitHandler());
+		MinecraftForge.EVENT_BUS.register(new TimeChecker());
 		GameRegistry.registerPlayerTracker(new ImpactPlayerTracker());
 		
+		final StepSound soundStoneFootstep = new StepSound("stone", 1.0F, 1.0F);
+		int i = Block.furnaceIdle.blockID;
+		Block.blocksList[i] = null;
+		Block ImpactBlockFurnace = (new ImpactBlockFurnace(61, false)).setHardness(3.5F).setStepSound(soundStoneFootstep).setUnlocalizedName("furnace").setCreativeTab(CreativeTabs.tabDecorations);
+		ReflectionHelper.setPrivateValue(Block.class, null, ImpactBlockFurnace, "furnace");
 		
 	}
 	@PostInit
